f5268edcfee12ef8fed71aea346cd7859377417b
Manideep <17101992m@gmail.com>
2023-02-04 22:35:03 +0530

    Dialog box will appear on cart button click

--- src/components/navbar/navbar.component.jsx
+++ src/components/navbar/navbar.component.jsx
@@ -1,12 +1,38 @@
+import { useState } from "react";
+
 const Navbar = () => {
+  const [dialogState, setDialogState] = useState(false);
+  const handleClick = () => {
+    dialogToggle()
+};
+
+const dialogToggle = () => {
+      setDialogState(!dialogState);
+  }
   return (
     <div className="flex justify-between flex-wrap py-4 px-8 shadow-gray-200 shadow-md">
       <span className="text-3xl font-semibold tracking-wide ">
         SHOPPING CART
       </span>
-      <button className="text-2xl tracking-wide font-semibold text-white rounded-full bg-orange-400 active:bg-orange-500 py-2 px-8">
+      <button
+        onClick={handleClick}
+        className="text-2xl tracking-wide font-semibold text-white rounded-full bg-orange-400 active:bg-orange-500 py-2 px-8"
+      >
         CART <span className="pl-2">6</span>
       </button>
+      {dialogState ? (
+        <dialog
+          class="open:bg-white border-2 p-6 rounded-lg shadow-gray-200 shadow-md backdrop:bg-gray-500 w-96"
+          open
+        >
+          <div className="flex justify-between text-3xl pb-2 border-b-2">
+            <span className="">Items in your Cart</span>
+            <button className="text-4xl font-bold" onClick={dialogToggle}>X</button>
+          </div>
+        </dialog>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
